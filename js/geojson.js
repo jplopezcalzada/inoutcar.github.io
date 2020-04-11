4d0a0d67786545fc7e9e03ac40d9cfb43827346b
Juan Pedro Pedro Lopez Calzada <ES00640464@emea.prosegur.local>
2020-04-11 12:58:02 +0200

    corregimos errores

--- js/geojson.js
+++ js/geojson.js
@@ -207,3 +207,12 @@
   }
 
 }(typeof module == 'object' ? module.exports : window.GeoJSON = {}));
+
+$(document).ready(function() {
+  $('#rootwizard').bootstrapWizard({onTabShow: function(tab, navigation, index) {
+  var $total = navigation.find('li').length;
+  var $current = index+1;
+  var $percent = ($current/$total) * 100;
+  $('#rootwizard .progress-bar').css({width:$percent+'%'});
+}});
+});
